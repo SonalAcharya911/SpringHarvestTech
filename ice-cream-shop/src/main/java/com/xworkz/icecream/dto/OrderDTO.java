212e162f6f105afb5871ffcbdfa8c4a1e841353c
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-25 01:32:02 +0530

    Initial commit with proper monorepo structure

---
+++ ice-cream-shop/src/main/java/com/xworkz/icecream/dto/OrderDTO.java
@@ -0,0 +1,10 @@
+package com.xworkz.icecream.dto;
+
+public class OrderDTO {
+    private String name;
+    private String flavour;
+    private int quantity;
+    private boolean takeAway;
+    private boolean addOn;
+    private String couponCode;
+}
