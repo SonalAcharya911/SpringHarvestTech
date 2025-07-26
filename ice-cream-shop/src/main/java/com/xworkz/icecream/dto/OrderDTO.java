294b8ca5d6638893edfdd6e1835698c538d7afc2
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-26 22:30:18 +0530

    ice cream shop

--- ice-cream-shop/src/main/java/com/xworkz/icecream/dto/OrderDTO.java
+++ ice-cream-shop/src/main/java/com/xworkz/icecream/dto/OrderDTO.java
@@ -1,5 +1,12 @@
 package com.xworkz.icecream.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class OrderDTO {
     private String name;
     private String flavour;
