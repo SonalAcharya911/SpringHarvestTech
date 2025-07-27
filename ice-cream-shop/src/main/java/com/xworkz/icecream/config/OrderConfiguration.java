80027e6ffb03359068380ff7caa48bfdfedf4c1a
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-27 23:48:14 +0530

    ice cream shop

--- ice-cream-shop/src/main/java/com/xworkz/icecream/config/OrderConfiguration.java
+++ ice-cream-shop/src/main/java/com/xworkz/icecream/config/OrderConfiguration.java
@@ -1,12 +1,24 @@
 package com.xworkz.icecream.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("com.xworkz.icecream")
 public class OrderConfiguration {
     public OrderConfiguration(){
         System.out.println("created order configuration...");
+    }
+
+    @Bean
+    public InternalResourceViewResolver registerInternalResourceViewResolver(){
+        System.out.println("registering InternalResourceViewResolver with spring container");
+        InternalResourceViewResolver internalResourceViewResolver=new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
     }
 }
