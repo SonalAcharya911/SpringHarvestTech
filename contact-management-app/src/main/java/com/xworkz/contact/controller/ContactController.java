56c2cd2ac50c0d7e73cde4d4b5c4f2eaaab49dfd
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-31 23:28:21 +0530

    contact app using spring mvc arch

--- contact-management-app/src/main/java/com/xworkz/contact/controller/ContactController.java
+++ contact-management-app/src/main/java/com/xworkz/contact/controller/ContactController.java
@@ -1,7 +1,8 @@
 package com.xworkz.contact.controller;
 
-import com.xworkz.contact.config.ContactConfiguration;
 import com.xworkz.contact.dto.ContactDTO;
+import com.xworkz.contact.service.ContactServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class ContactController {
+    @Autowired
+    ContactServiceImpl contactServiceImpl;
+
     public ContactController(){
         System.out.println("created ContactController..");
     }
@@ -17,6 +21,15 @@ public class ContactController {
     public String onSubmit(ContactDTO contactDTO, Model model){
         System.out.println("running onSubmit in ContactController...");
         model.addAttribute("dto",contactDTO);
+
+        System.out.println(contactDTO+" dto...........");
+
+        boolean valid= contactServiceImpl.validate(contactDTO);
+        if(valid){
+            System.out.println("saving success");
+            return "ContactSuccess.jsp";
+        }
+
         return "Contact.jsp";
     }
 }
