9891ba3db47f1241034537db5099843a2488aac2
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-02 00:00:34 +0530

    working with jpa

---
+++ diary-entries/src/main/java/com/xworkz/diary/service/ApplicationServiceImpl.java
@@ -0,0 +1,93 @@
+package com.xworkz.diary.service;
+
+import com.xworkz.diary.entity.ApplicationEntity;
+import com.xworkz.diary.repository.ApplicationRepository;
+import com.xworkz.diary.repository.ApplicationRepositoryImpl;
+
+public class ApplicationServiceImpl implements ApplicationService{
+
+    @Override
+    public boolean validate(ApplicationEntity entity) {
+        System.out.println("running validate in ApplicationServiceImpl");
+
+        if(entity!=null){
+            System.out.println("entity is not null");
+            if(entity.getApplicationName()!=null){
+                System.out.println("valid name");
+            }
+            else{
+                System.out.println("invalid name");
+                return false;
+            }
+            if(entity.getApplicationSize()!=null){
+                System.out.println("valid size");
+            }
+            else{
+                System.out.println("invalid size");
+                return false;
+            }
+
+            if(entity.getCompany()!=null){
+                System.out.println("valid company");
+            }
+            else{
+                System.out.println("invalid company");
+                return false;
+            }
+
+            if(entity.getRating()!=0){
+                System.out.println("valid rating");
+            }
+            else{
+                System.out.println("rating cannot be zero");
+                return false;
+            }
+
+            if(entity.getLaunchDate()!=null){
+                System.out.println("valid date");
+            }
+            else{
+                System.out.println("invalid date");
+                return false;
+            }
+
+            if(entity.getNoOfUsers()!=null){
+                System.out.println("valid number of users");
+            }
+            else{
+                System.out.println("invalid number of users");
+                return false;
+            }
+
+            ApplicationRepository applicationRepository=new ApplicationRepositoryImpl();
+            applicationRepository.saveApplication(entity);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public ApplicationEntity findByID(int id) {
+
+        ApplicationRepository repo=new ApplicationRepositoryImpl();
+        ApplicationEntity entity=repo.readByID(id);
+
+        return entity;
+    }
+
+    @Override
+    public void deleteByID(int id) {
+        ApplicationRepository repository=new ApplicationRepositoryImpl();
+        repository.deleteByID(id);
+    }
+
+    @Override
+    public ApplicationEntity findByApplicationName(String name) {
+        ApplicationEntity entity=null;
+
+        ApplicationRepository repo=new ApplicationRepositoryImpl();
+        entity=repo.findByApplicationName(name);
+
+        return entity;
+    }
+}
