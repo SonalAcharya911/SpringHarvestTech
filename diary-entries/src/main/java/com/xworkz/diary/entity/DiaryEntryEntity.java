9ecdafc4093a5e42b3d22e414f711c563ced5119
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-02 00:01:09 +0530

    working with jpa

---
+++ diary-entries/src/main/java/com/xworkz/diary/entity/DiaryEntryEntity.java
@@ -0,0 +1,11 @@
+package com.xworkz.diary.entity;
+
+import java.sql.Date;
+
+public class DiaryEntryEntity {
+    private int entryID;
+    private Date entryDate;
+    private String entryLines;
+
+
+}
