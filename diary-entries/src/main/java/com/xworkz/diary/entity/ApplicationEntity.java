d8d3f72f0ee1f53c7d19276cece58af0f002f2c7
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-02 00:00:42 +0530

    working with jpa

---
+++ diary-entries/src/main/java/com/xworkz/diary/entity/ApplicationEntity.java
@@ -0,0 +1,47 @@
+package com.xworkz.diary.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "application_info")
+
+@NamedQuery(name="findByApplicationName",query="select a from ApplicationEntity a where a.applicationName=:appName")
+@NamedQuery(name="findByAppSize", query="select a from ApplicationEntity a where a.applicationSize =: appSize")
+@NamedQuery(name="find", query="select a from ApplicationEntity a where a.:property =: appSize")
+
+
+
+
+
+public class ApplicationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "application_id")
+    private Integer applicationID;
+
+    @Column(name = "application_name")
+    private String applicationName;
+
+    @Column(name = "application_size")
+    private String applicationSize;
+
+    @Column(name = "company")
+    private String company;
+
+    @Column(name = "no_of_users")
+    private Integer noOfUsers;
+
+    @Column(name = "rating")
+    private Float rating;
+
+    @Column(name = "launch_date")
+    private LocalDate launchDate;
+}
