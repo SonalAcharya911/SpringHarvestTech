f0254486a93b828d18950b6ea98bc4b8feb7c3b6
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-09 21:26:04 +0530

    accepting file and sending an email

---
+++ exam-application/src/main/java/com/xworkz/exam/config/StudentInitializer.java
@@ -0,0 +1,41 @@
+package com.xworkz.exam.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+import java.io.File;
+
+public class StudentInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    private int maxUploadSizeInMb = 5 * 1024 * 1024;
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{StudentConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/register","/download","/sendMail"};
+    }
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+
+
+        // upload temp file will put here
+        File uploadDirectory = new File(System.getProperty("java.io.tmpdir"));
+
+        // register a MultipartConfigElement
+        MultipartConfigElement multipartConfigElement =
+                new MultipartConfigElement(uploadDirectory.getAbsolutePath(),
+                        maxUploadSizeInMb, maxUploadSizeInMb * 2, maxUploadSizeInMb / 2);
+
+        registration.setMultipartConfig(multipartConfigElement);
+
+    }
+}
