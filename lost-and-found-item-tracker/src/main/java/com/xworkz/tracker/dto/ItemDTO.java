793b5d53d3089d42e3e7c1348004a6b1c5f602c6
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-10 23:57:04 +0530

    integrating spring and jpa

---
+++ lost-and-found-item-tracker/src/main/java/com/xworkz/tracker/dto/ItemDTO.java
@@ -0,0 +1,14 @@
+package com.xworkz.tracker.dto;
+
+import java.sql.Date;
+
+public class ItemDTO {
+    private String holderName;
+    private String itemName;
+    private String itemDescription;
+    private String lostOrFound;
+    private String location;
+    private Date lostOrFoundDate;
+    private String email;
+    private Long contact;
+}
