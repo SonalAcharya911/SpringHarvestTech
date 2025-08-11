dc6cea2b35b74dc611ef9675731d605d6eb272d2
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-11 23:44:16 +0530

    integrating spring with jpa

--- tourism-management-app/src/main/java/com/xworkz/tourism/service/TourismService.java
+++ tourism-management-app/src/main/java/com/xworkz/tourism/service/TourismService.java
@@ -3,7 +3,11 @@ package com.xworkz.tourism.service;
 import com.xworkz.tourism.dto.TourismDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 public interface TourismService {
     boolean saveTourism(TourismDTO dto);
+
+    List<TourismDTO> fetchAll();
 }
