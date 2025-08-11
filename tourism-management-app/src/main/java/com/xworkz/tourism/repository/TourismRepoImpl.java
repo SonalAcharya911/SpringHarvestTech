0f9a67ee104f7edbdeaf42d8bca07b65d66bd068
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-11 23:44:09 +0530

    integrating spring with jpa

--- tourism-management-app/src/main/java/com/xworkz/tourism/repository/TourismRepoImpl.java
+++ tourism-management-app/src/main/java/com/xworkz/tourism/repository/TourismRepoImpl.java
@@ -7,12 +7,15 @@ import com.xworkz.tourism.service.TourismServiceImpl;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
+import java.util.Collections;
+import java.util.List;
 
 @Repository
 public class TourismRepoImpl implements TourismRepo{
     private static final EntityManagerFactory emf= Persistence.createEntityManagerFactory("x-workz");
     @Override
     public boolean saveTourism(TourismEntity entity) {
+        System.out.println("running saveTourism in TourismRepoImpl" );
 
         if(entity!=null){
             EntityManager em=null;
@@ -45,5 +48,34 @@ public class TourismRepoImpl implements TourismRepo{
             return  true;
         }
         return false;
+    }
+
+    @Override
+    public List<TourismEntity> fetchAll() {
+        System.out.println("running fetchAll");
+        List<TourismEntity> list=null;
+
+        EntityManager em=null;
+
+        try{
+            em= emf.createEntityManager();
+            list=em.createNamedQuery("fetchAll").getResultList();
+
+        }
+        catch(PersistenceException e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if(em!=null){
+                em.close();
+            }
+        }
+        return list;
+    }
+
+    public static  void closeFactory(){
+        if(emf!=null && emf.isOpen()){
+            emf.close();
+        }
     }
 }
