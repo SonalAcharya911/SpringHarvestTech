0492137758666f40b0ef66df1bf821257b7cb3e6
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-11 23:44:02 +0530

    integrating spring with jpa

--- tourism-management-app/src/main/java/com/xworkz/tourism/repository/TourismRepo.java
+++ tourism-management-app/src/main/java/com/xworkz/tourism/repository/TourismRepo.java
@@ -2,7 +2,11 @@ package com.xworkz.tourism.repository;
 
 import com.xworkz.tourism.entity.TourismEntity;
 
+import java.util.List;
+
 public interface TourismRepo {
     boolean saveTourism(TourismEntity entity);
+
+    List<TourismEntity> fetchAll();
 
 }
