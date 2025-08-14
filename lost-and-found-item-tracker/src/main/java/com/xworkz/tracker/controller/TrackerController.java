c42af1a51e2b8fc38921db9c2dd47173b8afe130
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-14 23:51:33 +0530

    integrating spring with jpa

--- lost-and-found-item-tracker/src/main/java/com/xworkz/tracker/controller/TrackerController.java
+++ lost-and-found-item-tracker/src/main/java/com/xworkz/tracker/controller/TrackerController.java
@@ -1,31 +1,47 @@
 package com.xworkz.tracker.controller;
 
 import com.xworkz.tracker.dto.ItemDTO;
+import com.xworkz.tracker.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+@Controller
 public class TrackerController {
+
+    @Autowired
+    private ItemService service;
     public TrackerController(){
         System.out.println("created TrackerController...");
     }
 
-    @GetMapping("")
+    @GetMapping("redirectToLostAndFound")
     public String redirectToLostAndFound(){
         System.out.println("redirecting to item lostAndFound");
-        return "/lostAndFound.jsp";
+        return "lostAndFound";
     }
 
+    @PostMapping("onSubmit")
     public String onSubmit(ItemDTO dto, Model model){
 
         System.out.println(dto);
         if(dto!=null){
 
-            boolean valid=false;
+            boolean valid=service.save(dto);
             if(valid){
-                System.out.println("data is valid");
+
                 System.out.println("Controller: "+dto);
-                model.addAttribute("success","success");
+                model.addAttribute("success","saved successfully");
                 System.out.println("data saved into database");
+                List<ItemDTO> dtoList=service.getAllItems();
+                System.out.println("Dto list in Controller: "+dtoList);
+                model.addAttribute("dtoList",dtoList);
+
+                return "ListOfItems";
             }
             else{
                 System.out.println("validation gone wrong");
@@ -33,6 +49,33 @@ public class TrackerController {
             }
         }
         model.addAttribute("dto",dto);
-        return "/index.jsp";
+        return "lostAndFound";
+    }
+
+    @GetMapping("getAllItems")
+    public String getAllItems(Model model){
+        List<ItemDTO> dtoList=service.getAllItems();
+        System.out.println("Dto list in Controller: "+dtoList);
+        model.addAttribute("dtoList",dtoList);
+
+        return "ListOfItems";
+    }
+
+
+    @GetMapping("backToIndex")
+    public String backToIndex(){
+        return "index";
+    }
+
+    @GetMapping("findByID")
+    public String findByID(Integer id,Model model){
+        System.out.println("running findByID in Controller");
+        if(id!=null){
+            System.out.println("id not null");
+            ItemDTO dto=service.findByID(id);
+            System.out.println("dto: "+dto);
+            model.addAttribute("dto",dto);
+        }
+        return "ListOfItems";
     }
 }
