5cc710ba8249a64b5e49f64f749a2787d4904af2
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-14 23:51:26 +0530

    integrating spring with jpa

--- lost-and-found-item-tracker/src/main/java/com/xworkz/tracker/config/TrackerConfiguration.java
+++ lost-and-found-item-tracker/src/main/java/com/xworkz/tracker/config/TrackerConfiguration.java
@@ -1,12 +1,23 @@
 package com.xworkz.tracker.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "com.xworkz.tracker")
 public class TrackerConfiguration {
     public TrackerConfiguration(){
         System.out.println("created TrackerConfiguration...");
+    }
+
+    @Bean
+    public InternalResourceViewResolver resolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/");
+        internalResourceViewResolver.setSuffix(".jsp");
+
+        return internalResourceViewResolver;
     }
 }
