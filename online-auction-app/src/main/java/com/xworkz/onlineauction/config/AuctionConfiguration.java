e9c91940e629483802e147bc23fa9b110e42fc0f
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-20 23:55:39 +0530

    spring project

--- online-auction-app/src/main/java/com/xworkz/onlineauction/config/AuctionConfiguration.java
+++ online-auction-app/src/main/java/com/xworkz/onlineauction/config/AuctionConfiguration.java
@@ -1,10 +1,12 @@
 package com.xworkz.onlineauction.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@ComponentScan(basePackages = "com.xworkz.onlineauction")
 public class AuctionConfiguration {
 
     public AuctionConfiguration(){
--- online-auction-app/src/main/java/com/xworkz/onlineauction/repository/AuctionRepository.java
+++ online-auction-app/src/main/java/com/xworkz/onlineauction/repository/AuctionRepository.java
@@ -4,4 +4,8 @@ import com.xworkz.onlineauction.entity.MemberEntity;
 
 public interface AuctionRepository {
     boolean save(MemberEntity memberEntity);
+
+    boolean checkEmailExist(String email);
+
+    boolean checkContactExist(Long contact);
 }
