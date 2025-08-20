1eb03b8d2d1311347dbdd67f1f13cba4d32b6959
SonalAcharya911 <sonalacharya911@gmail.com>
2025-08-20 23:55:19 +0530

    integrating spring with jpa

---
+++ tourism-management-app/src/main/java/com/xworkz/tourism/restcontroller/MemberLoginRestController.java
@@ -0,0 +1,12 @@
+package com.xworkz.tourism.restcontroller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberLoginRestController {
+    public MemberLoginRestController(){
+        System.out.println("created MemberLoginRestController");
+    }
+
+
+}
